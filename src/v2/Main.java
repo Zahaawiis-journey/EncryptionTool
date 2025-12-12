b7ecdafcb51180966d01109fb32528242eea0415
Zahaawii <zahaawii@gmail.com>
2025-12-12 17:14:39 +0100

    commenting the process for the encryption idea

---
+++ src/v2/Main.java
@@ -0,0 +1,12 @@
+package v2;
+
+public class Main {
+
+
+    /*
+
+    The idea with this project is to try to implement a new version two of my encryption process.
+    First time was without youtube videos - this time with youtube.
+
+     */
+}
