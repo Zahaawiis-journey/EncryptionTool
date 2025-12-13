25501d1d55260722287920b3ce207b9ebe65cfcb
Zahaawii <zahaawii@gmail.com>
2025-12-13 23:19:59 +0100

    init v2 with the methods to create key, and send message

---
+++ src/v2/Encryption.java
@@ -0,0 +1,24 @@
+package v2;
+
+import java.util.Scanner;
+
+public class Encryption {
+
+    private String message;
+    private String encryptionKey;
+    private Scanner scanner = new Scanner(System.in);
+
+
+
+    private String getKey() {
+        return null;
+    }
+
+    private String newKey() {
+        return null;
+    }
+
+    private String sendMessage() {
+        return "message";
+    }
+}
