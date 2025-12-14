ae3a4f8ba9b930ea3206ef9d0a96955b66e6acce
Zahaawii <zahaawii@gmail.com>
2025-12-14 21:03:19 +0100

    making progress with adding variables to the v2. Not quite sure what I am going to do, so Im wathicng a youtube video

--- src/v2/Encryption.java
+++ src/v2/Encryption.java
@@ -1,28 +1,45 @@
 package v2;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Encryption {
 
     private String message;
     private String encryptionKey;
-    private Scanner scanner = new Scanner(System.in);
+    private Random random;
+    private Scanner scanner;
+    private ArrayList<Character> list;
+    private ArrayList<Character> shuffledList;
+    private char character;
+    private String line;
+    private char[] letters;
+    private char[] secretLetters;
 
+    public Encryption() {
+        random = new Random();
+        scanner = new Scanner(System.in);
+        list = new ArrayList<>();
+        shuffledList = new ArrayList<>();
+        character = ' ';
 
-
-    private String getKey() {
-        return null;
+        newKey();
     }
 
-    private String newKey() {
-        return null;
+    private void getKey() {
     }
 
-    private String encryptMessage() {
-        return null;
+    private void newKey() {
     }
 
-    private String sendMessage() {
-        return "message";
+    private void encryptMessage() {
+    }
+
+    private void decryptMessage() {
+
+    }
+
+    private void sendMessage() {
     }
 }
